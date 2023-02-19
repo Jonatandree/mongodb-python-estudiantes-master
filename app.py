62eab5252c664568a70e28218513273a8be9fff5
jonatandree <javasqueza@unah.hn>
2023-02-19 14:01:14 -0600

    conectando mongodb con nuestro programa

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
+import pymongo
 from classes import Estudiante
-from dotenv import load_dotenv
+
 
 def main():
-    estudiante = Estudiante("Uayeb 3","Caballero","31487539")
-    estudiante.save()
     
+    user = "AndreeVasquez"
+    password = "cocorasta"
+    cluster ="cluster0.hfac6vs.mongodb.net"
+    query_string = "retryWrites=true&w=majority"
+
+    ## Connection String
+    uri = "mongodb+srv://{0}:{1}@{2}/?{3}".format(
+        user
+        , password
+        , cluster
+        , query_string
+    )
+
+    client = pymongo.MongoClient(uri)
+    db = client['unah']
+    collection = db['estudiante']
+    estudiante = Estudiante("andree","vasquez","31487539")
+    print(estudiante.__dict__)
+    collection.insert_one( estudiante.__dict__ )
+
 if __name__ == "__main__":
-    load_dotenv()
     main()
 
 
 
+#collection.insert_one({ "nombre": "Ixchel", "telefono": "eeee" })
+
+#for document in collection.find():
+    #print(document)
 
--- classes/__init__.py
+++ classes/__init__.py
@@ -1,2 +1,1 @@
 from .Estudiante import *
-from .DbMongo import *
